5798d9797b4cbbdb9dc7535bea185b778e1dc574
Jeem312 <Shajidajeem312@gmail.com>
2024-07-16 20:09:27 +0600

    mongodb setup

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ require('dotenv').config();
 const cors = require('cors');
 const app = express();
 const jwt = require('jsonwebtoken');
-const port = process.env.PORT || 5000;
+const bcrypt = require('bcrypt');
+const port = process.env.PORT || 7000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 // Middleware
 app.use(cors());
@@ -29,11 +30,50 @@ async function run() {
   try {
 
     const userCollection = client.db("FinMob").collection('users');
+
+
+
+
+
+    app.post('/register', async (req, res) => {
+        const { 
+          name,
+          email
+          , pin
+          ,role,balance,status } = req.body;
+        const hashedPassword = await bcrypt.hash(pin, 10);
+        const newUser = { name, email, pin: hashedPassword,role,balance, status};
+        
+        try {
+          await userCollection.insertOne(newUser);
+          res.status(201).json({ message: 'registered successfully' });
+        } catch (error) {
+          res.status(500).json({ message: 'Error registering user' });
+        }
+      });
+
+      app.post('/login', async (req, res) => {
+        const { email, pin } = req.body;
+        const user = await userCollection.findOne({ email });
+        
+        if (!user) {
+          return res.status(400).json({ message: 'User not found' });
+        }
+      
+        const isMatch = await bcrypt.compare(pin, user.pin);
+        if (!isMatch) {
+          return res.status(400).json({ message: 'Invalid credentials' });
+        }
+      
+        const token = jwt.sign({ email: user.email }, process.env.SECRET_KEY, { expiresIn: '365d' });
+        res.json({ success: true, token });
+      });
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -41,7 +81,11 @@ async function run() {
 }
 run().catch(console.dir);
 
-
+// Define a basic route
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+  });
+  
 app.listen(port, () => {
   console.log(`'Server is running on port:' ${port}`);
 });
